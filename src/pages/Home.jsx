3c8c922a4917d0a1d15c12fa611e5bec7ab0f528
Yogesh <107619666+GitFlow-Yogesh@users.noreply.github.com>
2025-12-22 17:46:05 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import myPhoto from "../assets/image.png";
+import myPhoto from "../assets/photo_updated.jpeg";
 
 function Home() {
   return (
